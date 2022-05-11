498c38c7814b72a47a0af2d1aaf667c3de665d1c
Dominik Schallmeiner <schaldom@students.zhaw.ch>
2022-05-11 09:29:36 +0200

    Added v2 in res.sen server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello Wednesday DevOps Course! Tom, Max, Dominik ;.');
+	res.send('Hello Wednesday DevOps Course! Tom, Max, Dominik ;.v2');
 });
 
 app.listen(PORT, HOST);
