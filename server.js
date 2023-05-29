292f3cb953cd1bce4acf51bdffbb3f37f5abdd91
huberden <91844408+huberden@users.noreply.github.com>
2023-05-29 15:49:07 +0200

    DevOpsDocker

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello Monday 16.05.2022 DevOps Course! Tom, Max, Dominik v5');
+	res.send('Hello Dennis to the DevOps Course!');
 });
 
 app.listen(PORT, HOST);
